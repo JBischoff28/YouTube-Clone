e19e30e5aa12fc1c8268b5cc2990bff0ac9b94b7
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-02-18 22:35:41 -0500

    Removed useEffect from SearchPage temporarily due to it using my entire Youtube API quota. Will Reimplement when the feature is needed. Finished creating SearchBar component. TODO: Test SearchBar component when daily API quota resets.

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
-
-const [search, setSearch] = useState('');
+import axios from "axios";
 
 const SearchBar = (props) => {
+
+    const [search, setSearch] = useState('');
+
+    async function searchVideos (search) {
+        try {
+            let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${search}&key=AIzaSyDPGOO0T4jFJdR0PLayq6N6Wum3OoW8n58&part=snippet&type=video&maxResults=6`);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(search);
+        searchVideos(search)
+    }
+
     return ( 
-        <form className="searchForm">
+        <form onSubmit={(event) => handleSubmit(event)} className="searchForm">
             <ul>
                 <li>
                     <label>Search for videos here!</label>
                 </li>
                 <li>
-                    <input type='text' value={search} onChange={(event) => {event.target.value}}/>
+                    <input type='text' value={search} onChange={(event) => setSearch(event.target.value)}/>
                 </li>
-                <li></li>
+                <li>
+                    <button type='submit'>SEARCH!</button>
+                </li>
             </ul>
         </form>
      );
--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { KEY } from "../../localKey.js"
+
+//Component Imports
+import SearchBar from "../../components/SearchBar/SearchBar.jsx";
 
 const SearchPage = () => {
 
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const fetchVideos = async () => {
-      try {
-        let response = await axios.get("");
-        setVideos(response.data)
-      } catch (error) {
-        console.log(error.message)
-      }
+  async function fetchVideos () {
+    try {
+      let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${KEY}&part=snippet&type=video&maxResults=6`);
+      setVideos(response.data)
+    } catch (error) {
+      console.log(error.message)
     }
-  }, [])
+  }
 
   return (
     <div className="container">
-      <h1></h1>
+      <SearchBar />
     </div>
   );
 };
