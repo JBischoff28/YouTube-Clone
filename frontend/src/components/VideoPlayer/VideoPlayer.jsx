948c25da11e762a31b7ccf7bcef7cc802d07ffb5
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-02-20 18:01:46 -0500

    SAVING PROGRESS: Modified SearchPage and VideoPlayer when trying to figure out how to pass a video's videoId to the VideoPlayer.

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 const VideoPlayer = (props) => {
     return (
         <div>
-            <iframe src={`https://www.youtube.com/embed/${props.videos.videoId}`}>
+            <iframe src={`https://www.youtube.com/embed/${props.vids}`}>
             </iframe>
         </div>
      );
--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -12,10 +12,12 @@ const SearchPage = () => {
   const [videos, setVideos] = useState([]);
 
   async function searchVideos (search) {
+    setVideos([]);
     try {
         let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${search}&key=${KEY2}&part=snippet&type=video&maxResults=6`);
         console.log(response.data);
         setVideos(response.data);
+        console.log(videos);
     } catch (error) {
         console.log(error.message);
     }
@@ -34,14 +36,7 @@ const SearchPage = () => {
   return (
     <div className="container">
       <SearchBar searchVideos={searchVideos}/>
-      <div className="videoTray">
-        <VideoPlayer videos={videos}/>
-        <VideoPlayer videos={videos}/>
-        <VideoPlayer videos={videos}/>
-        <VideoPlayer videos={videos}/>
-        <VideoPlayer videos={videos}/>
-        <VideoPlayer videos={videos}/>
-      </div>
+      <VideoPlayer />
     </div>
   );
 };
