7e59a4994241b0075d45f7e86f81b830d8b1a892
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-02-24 23:24:38 -0500

    Added async function to VideoPage that will serve to send a GET request to the Youtube Data API for three videos related to the currently selected video. Added useEffect to the VideoPage that will call the GetRelatedVideos async function when the page is loaded.

--- frontend/src/pages/VideoPage/VideoPage.js
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from "react-router-dom";
+
+//API Keys
+import { KEY } from "../../localKey.js";
+import { KEY2 } from "../../localKey.js";
 
 //Component Imports
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
@@ -8,12 +13,26 @@ import CommentList from '../../components/CommentList/CommentList';
 const VideoPage = () => {
 
     const { videoId } = useParams();
+    const [relatedVideos, setRelatedVideos] = useState([]);
+
+    useEffect(() => {
+        //GetRelatedVideos();
+    }, []);
+
+    async function GetRelatedVideos() {
+        try {
+            const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=3&key=${KEY}}&relatedToVideoId=${videoId}`);
+            console.log(response.data.items);
+            setRelatedVideos(response.data.items);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return ( 
         <div className="container">
-            <h1>Watch Me!</h1>
             <CommentList />
-            <VideoPlayer videoId={videoId}/>
+            <VideoPlayer videoId={videoId} />
         </div>
      );
 }
